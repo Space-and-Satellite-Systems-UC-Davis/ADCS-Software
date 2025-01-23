285175944beb6f1ff801f6535a9384a1f736d2e3
Tarini Maram <56371094+tarinimaram@users.noreply.github.com>
2025-01-22 16:07:39 -0800

    integrate sensor calibration with virtual intellisat interface

--- adcs_math/sensors.h
+++ adcs_math/sensors.h
@@ -16,7 +16,7 @@
  * 
  * @param currValue current sensor raw value
  * @param prevValue previous sensor raw value
- * @param filterConstant constant between 0-1; a lower value is a greater damp on unusually large jumps in sensor value
+ * @param filterConstant constant between 0-1; a greater value is a greater damp on unusually large jumps in sensor data
  * 
  * @return filtered sensor value
 */
@@ -25,14 +25,16 @@ float lowpass_filter(float currValue, float prevValue, float filterConstant);
 /**
  * @brief Calculates sensor calibration value after filtering data through simple lowpass filter with default constant 0.5
  * 
+ * @param sensor type of sensor defined by VI enum
  * @param currValue current sensor raw value
  * @param prevValue previous sensor raw value
  * @param offset constant the sensor reads when it is not supposed to read anything
  * @param gain constant multiplier for sensor raw values
+ * @param filterConstant attenuation constant between 0-1 for lowpass filter; a greater value is a greater damp on unusually large jumps in sensor data
  * 
  * @return sensor calibration value after filtration 
 */
-float get_sensor_calibration(float currValue, float prevValue, float offset, float gain);
+float get_sensor_calibration(vi_sensors sensor, float currValue, float prevValue, float offset, float gain, float filterConstant);
 
 /**
  * @brief safely calculate delta_t accounting for integer overflow
