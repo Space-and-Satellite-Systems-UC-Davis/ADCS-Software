53670fb0d9bff0ff40f91b333cf3f47141026d08
jacobtkeio <jacobtkeio@gmail.com>
2025-01-29 15:47:22 -0800

    Add ramp experiment

---
+++ control/experiment/ramp_experiment.c
@@ -0,0 +1,37 @@
+
+#include "ramp_experiment.h"
+
+run_ramp_experiment_status ramp_experiment(){
+
+    ramp_controller controller;
+    int t0;
+    vi_hdd_command_status command_status;
+    vi_get_curr_millis_status millis_status = vi_get_curr_millis(&t0);
+    if (millis_status == GET_CURR_MILLIS_FAILURE){
+        return RUN_RAMP_EXPERIMENT_FAILURE;
+    }
+
+    init_ramp_controller(t0, 1000, 1000, 2000, 1.0, &controller);
+    int ti = t0;
+
+    while((ti-t0) < controller.left_leg_length_ms 
+    + controller. right_leg_length_ms 
+    + controller.plateau_length_ms){
+
+        millis_status = vi_get_curr_millis(&ti);
+        if (millis_status == GET_CURR_MILLIS_FAILURE){
+            return RUN_RAMP_EXPERIMENT_FAILURE;
+        }
+
+        double command = linear_ramp_command(ti , &controller);
+        
+        command_status = vi_hdd_command(command);
+        if (command_status == HDD_COMMAND_FAILURE){
+            return RUN_RAMP_EXPERIMENT_FAILURE;
+        }
+
+    }
+    
+    return RUN_RAMP_EXPERIMENT_SUCCESS;
+    
+}
