f08d5e1206d5065d5f3ceb6a997fed1f643eeb92
Tarini Maram <56371094+tarinimaram@users.noreply.github.com>
2025-02-12 17:02:56 -0800

    finish determination experiment

--- control/experiment/determination_experiment.c
+++ control/experiment/determination_experiment.c
@@ -1,6 +1,7 @@
 #include "determination_experiment.h"
 #include "determination.h"
 #include "vector.h"
+#include "sensors.h"
 
 determination_exp_status determination_experiment()
 {
@@ -8,46 +9,62 @@ determination_exp_status determination_experiment()
     mat3 currAttitude;
     int year, month, day, hour, minute, second;
     vec3 mag;
-    // have to calculate/get measured_sun (from where?)
     vec3 sun;
 
     vi_get_epoch(&year, &month, &day, &hour, &minute, &second);
     vi_get_mag(&mag.x, &mag.y, &mag.z);
     
-    // default values for now, waiting for sun sensors to implement get_sun
+    //TODO: default values for now, waiting for sun sensors to implement get_sun
     sun.x = 0;
     sun.y = 0;
     sun.z = 0;
 
-    determination(year, month, day, hour, minute, second, mag, sun, &currAttitude);
+    determination(year, month, day, hour, minute, second, mag, sun, &prevAttitude);
 
-    //Get current angular velocity for z axis
-    double angvel_x = 0, angvel_y = 0, angvel_z = 0;
-    if(vi_get_angvel(&angvel_x, &angvel_y, &angvel_z) == GET_ANGVEL_FAILURE)
-        return DETERMINATION_EXPERIMENT_FAILURE;
+    //TODO: generalizing initial angular velocity as 0; might have to fix
+    int angvel_z = 0;
 
     //Get the current time (Virtual Intellisat)
+    int prev_millis = 0;
     int curr_millis = 0;
-    if(vi_get_curr_millis(&curr_millis) == GET_CURR_MILLIS_FAILURE)
+    if(vi_get_curr_millis(&prev_millis) == GET_CURR_MILLIS_FAILURE)
         return DETERMINATION_EXPERIMENT_FAILURE;
 
     //Declare and initlialize PID controller
     PID_controller controller;
     double target = 0;
-    PID_init(target, angvel_z, curr_millis, 1, 1, 1, &controller);
+    PID_init(target, angvel_z, prev_millis, 1, 1, 1, &controller);
     
     //Run a while loop 
     while (abs(target - angvel_z) > 0.1)
     {
+        vi_delay_ms(100);
+        vi_get_epoch(&year, &month, &day, &hour, &minute, &second);
+        vi_get_mag(&mag.x, &mag.y, &mag.z);
+        // default values for now, waiting for sun sensors to implement get_sun
+        sun.x = 0;
+        sun.y = 0;
+        sun.z = 0;
+
+        determination(year, month, day, hour, minute, second, mag, sun, &currAttitude);
         //Get the current time (Virtual Intellisat)
         if(vi_get_curr_millis(&curr_millis) == GET_CURR_MILLIS_FAILURE)
             return DETERMINATION_EXPERIMENT_FAILURE;
+        
+        int delta_t = get_delta_t(curr_millis, prev_millis);
+        
+        mat3 derivative;
+        mat_sub(currAttitude, prevAttitude, &derivative);
+        mat_scalar(1.0/delta_t, derivative, &derivative);
+        double zrotation = derivative.y1;
 
         //PLug it into the control function
-        double throttle = PID_command(target, state, curr_millis, &controller);
+        double throttle = PID_command(target, zrotation, curr_millis, &controller);
         //Take output and plug it into HDD 
         if(vi_hdd_command(throttle) == HDD_COMMAND_FAILURE)
             return DETERMINATION_EXPERIMENT_FAILURE;
+        prevAttitude = currAttitude;
+        prev_millis = curr_millis;
     }
 
     return DETERMINATION_EXPERIMENT_SUCCESS;
