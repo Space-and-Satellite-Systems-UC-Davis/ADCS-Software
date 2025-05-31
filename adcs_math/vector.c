1773090caca38c884df0251918e1fa14379254fb
jacobtkeio <jacobtkeio@gmail.com>
2025-05-31 13:28:05 -0700

    Format adcs_math and determination

--- adcs_math/vector.c
+++ adcs_math/vector.c
@@ -7,64 +7,42 @@
  * 	without issue, even if some pass-by-value arguments are later
  * 	converted to pass-by-reference.
  *
- * @author Jacob Tkeio (jacobtkeio@gmail.com) 5/29/2023 
+ * @author Jacob Tkeio (jacobtkeio@gmail.com) 5/29/2023
  */
 
-#include <math.h>
 #include "vector.h"
-
+#include <math.h>
 
 void vec_set(double x, double y, double z, vec3 *output) {
-	(*output).x = x;
-	(*output).y = y;
-	(*output).z = z;
+    (*output).x = x;
+    (*output).y = y;
+    (*output).z = z;
 }
 
 void vec_scalar(double scalar, vec3 vector, vec3 *output) {
-	vec_set(
-		scalar * (vector.x),
-		scalar * (vector.y),
-		scalar * (vector.z),
-		output
-	);
+    vec_set(scalar * (vector.x), scalar * (vector.y), scalar * (vector.z),
+            output);
 }
 
 void vec_cross(vec3 left, vec3 right, vec3 *output) {
-	vec_set(
-		(left.y * right.z - left.z * right.y),
-		(left.z * right.x - left.x * right.z),
-		(left.x * right.y - left.y * right.x),
-		output
-	);
+    vec_set((left.y * right.z - left.z * right.y),
+            (left.z * right.x - left.x * right.z),
+            (left.x * right.y - left.y * right.x), output);
 }
 
 void vec_add(vec3 left, vec3 right, vec3 *output) {
-	vec_set(
-		left.x + right.x,
-		left.y + right.y,
-		left.z + right.z,
-		output
-	);
+    vec_set(left.x + right.x, left.y + right.y, left.z + right.z, output);
 }
 
 void vec_sub(vec3 left, vec3 right, vec3 *output) {
-    vec_set(
-        left.x - right.x,
-        left.y - right.y,
-        left.z - right.z,
-        output
-    );
+    vec_set(left.x - right.x, left.y - right.y, left.z - right.z, output);
 }
 
 void vec_bisect(vec3 left, vec3 right, vec3 *output) {
-	vec3 sum_vector;
-	vec_add(left, right, &sum_vector);
+    vec3 sum_vector;
+    vec_add(left, right, &sum_vector);
 
-	vec_scalar(
-		0.5,
-		sum_vector,
-		output
-	);
+    vec_scalar(0.5, sum_vector, output);
 }
 
 int vec_norm(vec3 vector, vec3 *output) {
@@ -74,27 +52,14 @@ int vec_norm(vec3 vector, vec3 *output) {
         return -1;
     }
 
-	vec_scalar(
-		1 / mag,
-		vector,
-		output
-	);
+    vec_scalar(1 / mag, vector, output);
 
     return 0;
 }
 
 double vec_dot(vec3 first, vec3 second) {
-	return
-		(first.x) * (second.x) +
-		(first.y) * (second.y) +
-		(first.z) * (second.z);
+    return (first.x) * (second.x) + (first.y) * (second.y) +
+           (first.z) * (second.z);
 }
 
-double vec_mag(vec3 vector) {
-	return sqrt(vec_dot(vector, vector));
-}
-
-
-
-
-
+double vec_mag(vec3 vector) { return sqrt(vec_dot(vector, vector)); }
--- determination/determination.c
+++ determination/determination.c
@@ -1,21 +1,21 @@
 #include "determination/determination.h"
 #include "determination/novasc3.1/novas.h"
 
+#include "determination/TRIAD/triad.h"
+#include "determination/mag_lookup/mag_lookup.h"
 #include "determination/pos_lookup/pos_lookup.h"
 #include "determination/sun_lookup/sun_lookup.h"
-#include "determination/mag_lookup/mag_lookup.h"
-#include "determination/TRIAD/triad.h"
 
-#include "adcs_math/vector.h"
 #include "adcs_math/matrix.h"
+#include "adcs_math/vector.h"
 
-#include "virtual_intellisat.h"
 #include "ADCS.h"
+#include "virtual_intellisat.h"
 
 typedef struct Determination_Cache {
     double last_update_time;
 
-    // for pos_lookup 
+    // for pos_lookup
     double longitude;
     double latitude;
     double altitude;
@@ -30,43 +30,35 @@ typedef struct Determination_Cache {
 
 static determination_cache cache;
 
-determination_status
-determination(
-    int year,
-    int month,
-    int day,
-    int hour,
-    int minute,
-    double second,
-    vec3 measured_mag,
-    vec3 measured_sun,
-    mat3 *attitude
-) {
+determination_status determination(int year, int month, int day, int hour,
+                                   int minute, double second, vec3 measured_mag,
+                                   vec3 measured_sun, mat3 *attitude) {
 
-    double UTC = julian_date(
-        year, month, day,
-        hour + minute/60.0 + second/3600.0
-    );
+    double UTC =
+        julian_date(year, month, day, hour + minute / 60.0 + second / 3600.0);
 
-    int recent_lookup = 0; //false
+    int recent_lookup = 0; // false
 
-    if ((UTC - cache.last_update_time) <= julian_date(0, 0, 0, 5/60.0)) {
-        recent_lookup = 1; //true
+    if ((UTC - cache.last_update_time) <= julian_date(0, 0, 0, 5 / 60.0)) {
+        recent_lookup = 1; // true
     } else {
         cache.last_update_time = UTC;
     }
-    
-    int update_IGRF = 0; //false
+
+    int update_IGRF = 0; // false
     char *tle_line1;
     char *tle_line2;
 
-    vi_get_TLE_status tle_status =
-    vi_get_TLE(tle_line1, tle_line2);
+    vi_get_TLE_status tle_status = vi_get_TLE(tle_line1, tle_line2);
 
     switch (tle_status) {
-        case GET_TLE_FAILURE: return DET_NO_TLE;
-        case GET_TLE_SUCCESS_OLD: break;
-        case GET_TLE_SUCCESS_NEW: update_IGRF = 1; break;
+    case GET_TLE_FAILURE:
+        return DET_NO_TLE;
+    case GET_TLE_SUCCESS_OLD:
+        break;
+    case GET_TLE_SUCCESS_NEW:
+        update_IGRF = 1;
+        break;
         // update IGRF when we get a new TLE, including the 1st time
     }
 
@@ -86,17 +78,8 @@ determination(
         pos_status = cache.pos_status;
     } else {
         pos_status =
-        pos_lookup(
-            tle_line1,
-            tle_line2,
-            UTC,
-            0.0,
-            &longitude,
-            &latitude,
-            &altitude,
-            &geocentric_radius,
-            &geocentric_latitude
-        );
+            pos_lookup(tle_line1, tle_line2, UTC, 0.0, &longitude, &latitude,
+                       &altitude, &geocentric_radius, &geocentric_latitude);
 
         cache.pos_status = pos_status;
         cache.longitude = longitude;
@@ -107,119 +90,88 @@ determination(
     }
 
     switch (pos_status) {
-        case SGP4_ERROR:            return DET_POS_LOOKUP_ERROR;
-        case TEME2ITRS_ERROR:       return DET_POS_LOOKUP_ERROR;
-        case ITRS2LLA_ERROR:        return DET_POS_LOOKUP_ERROR;
-        case POS_LOOKUP_SUCCESS:    break;
+    case SGP4_ERROR:
+        return DET_POS_LOOKUP_ERROR;
+    case TEME2ITRS_ERROR:
+        return DET_POS_LOOKUP_ERROR;
+    case ITRS2LLA_ERROR:
+        return DET_POS_LOOKUP_ERROR;
+    case POS_LOOKUP_SUCCESS:
+        break;
     }
 
-
-	vec3 reference_sun;
+    vec3 reference_sun;
     sun_lookup_status sun_status;
 
     if (recent_lookup) {
         reference_sun = cache.reference_sun;
         sun_status = cache.sun_status;
     } else {
-        sun_status =
-        sun_lookup(
-            longitude,
-            latitude,
-            altitude,
-            year,
-            month,
-            day,
-            hour,
-            minute,
-            second,
-            &reference_sun
-        );
+        sun_status = sun_lookup(longitude, latitude, altitude, year, month, day,
+                                hour, minute, second, &reference_sun);
 
         cache.sun_status = sun_status;
         cache.reference_sun = reference_sun;
     }
 
     switch (sun_status) {
-        case SUN_LOOKUP_BAD_DATE:       return DET_POS_LOOKUP_ERROR;
-        case SUN_LOOKUP_BAD_ENVIRONMENT:return DET_POS_LOOKUP_ERROR;
-        case SUN_LOOKUP_BAD_LLA:        return DET_POS_LOOKUP_ERROR;
-        case SUN_LOOKUP_SUCCESS:        break;
+    case SUN_LOOKUP_BAD_DATE:
+        return DET_POS_LOOKUP_ERROR;
+    case SUN_LOOKUP_BAD_ENVIRONMENT:
+        return DET_POS_LOOKUP_ERROR;
+    case SUN_LOOKUP_BAD_LLA:
+        return DET_POS_LOOKUP_ERROR;
+    case SUN_LOOKUP_SUCCESS:
+        break;
     }
-
 
     vec3 reference_mag;
 
     // Only update IGRF date before recalculating coeffs
     if (update_IGRF) {
         int igrf_time_status =
-        igrf_set_date_time(
-            year,
-            month,
-            day,
-            hour,
-            minute,
-            second
-        );
+            igrf_set_date_time(year, month, day, hour, minute, second);
 
         switch (igrf_time_status) {
-            //TODO: use 'default' approximate time if out of bounds?
-            case IGRF_SET_DATE_OUT_OF_BOUNDS:
-                return DET_IGRF_TIME_ERROR;
-            case IGRF_SET_DATE_SUCCESS:
-                break;
+        // TODO: use 'default' approximate time if out of bounds?
+        case IGRF_SET_DATE_OUT_OF_BOUNDS:
+            return DET_IGRF_TIME_ERROR;
+        case IGRF_SET_DATE_SUCCESS:
+            break;
         }
-    } 
+    }
 
     if (recent_lookup) {
         reference_mag = cache.reference_mag;
     } else {
-        igrf_update(
-            geocentric_latitude,
-            longitude,
-            geocentric_radius,
-            update_IGRF, //recalculate coefficients only on new TLE
-            &reference_mag
-        );
+        igrf_update(geocentric_latitude, longitude, geocentric_radius,
+                    update_IGRF, // recalculate coefficients only on new TLE
+                    &reference_mag);
         cache.reference_mag = reference_mag;
     }
 
-    triad_run_status triad_status = 
-    triad(
-    	measured_sun,
-        measured_mag,
-    	reference_sun,
-        reference_mag,
-    	attitude
-    );
+    triad_run_status triad_status = triad(
+        measured_sun, measured_mag, reference_sun, reference_mag, attitude);
 
     switch (triad_status) {
-        case TRIAD_NORM_FAILURE:    return DET_TRIAD_ERROR;
-        case TRIAD_SUCCESS:         break;
+    case TRIAD_NORM_FAILURE:
+        return DET_TRIAD_ERROR;
+    case TRIAD_SUCCESS:
+        break;
     }
 
     return DET_SUCCESS;
 }
 
-void get_earth_direction(
-    int year,
-    int month,
-    int day,
-    int hour,
-    int minute,
-    double second,
-    vec3 measured_mag,
-    vec3 measured_sun, 
-    vec3* earth_attitude) {
+void get_earth_direction(int year, int month, int day, int hour, int minute,
+                         double second, vec3 measured_mag, vec3 measured_sun,
+                         vec3 *earth_attitude) {
 
     mat3 attitude;
-    determination(year, month, day, hour, minute, second, measured_mag, measured_sun, &attitude);
+    determination(year, month, day, hour, minute, second, measured_mag,
+                  measured_sun, &attitude);
 
     vec3 down;
     vec_set(0, 0, 1, &down);
     mat_vec_mult(attitude, down, earth_attitude);
 }
-
-
-
-
-
