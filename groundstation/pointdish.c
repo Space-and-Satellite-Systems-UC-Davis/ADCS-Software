c5469995501d49243f58f302ecf77a4151a23270
Ryan T. <ryantengan2004@gmail.com>
2025-08-09 14:22:20 -0700

    PS data config file, groundstation elevation for further accuracy of point_dish calculations

--- groundstation/pointdish.c
+++ groundstation/pointdish.c
@@ -7,16 +7,16 @@
 #include <math.h>
 
 
-void point_dish(double gs_lat, double gs_lon, vec3 sat_pos_ecef, double *dish_az, double *dish_el){
-    double R_earth = 6371.0; // Could customize to our particular ground station, although that might be overkill
+void point_dish(double gs_lat, double gs_lon, double gs_el, vec3 sat_pos_ecef, double *dish_az, double *dish_el){
+    double R_total = 6371.0 + gs_el; // Radius of earth + elevation of groundstation
 
     gs_lat = gs_lat * (M_PI/180.0);
     gs_lon = gs_lon * (M_PI/180.0);
 
     vec3 gs_pos_ecef;
-    vec_set(R_earth * cos(gs_lat) * cos(gs_lon),
-            R_earth * cos(gs_lat) * sin(gs_lon),
-            R_earth * sin(gs_lat),
+    vec_set(R_total * cos(gs_lat) * cos(gs_lon),
+            R_total * cos(gs_lat) * sin(gs_lon),
+            R_total * sin(gs_lat),
             &gs_pos_ecef);
 
     vec3 diff_vector_ecef; // Can also be thought of as the satellite's position in earth fixed gs centered coordinates
@@ -90,21 +90,30 @@ int main() {
  
     // { echo "38.53"; echo "121.76"; curl -s 'https://tle.ivanstanojevic.me/api/tle/25544' | jq '.line1, .line2' | tr -d '"'; curl -s "https://ssd-api.jpl.nasa.gov/jd_cal.api?cd=$(date +%F_%T)" | jq -r '.jd'; } | ./pointdish
  
-    char lines[5][100]; //pipe to here 
+    char lines[4][100]; //pipe to here 
 
     double az;
-    double el; 
+    double el_dish;
 
+    FILE *fp = fopen( "PS_data.txt","r");
+    if(fp == NULL){
+        perror("Error opening file");
+        return;
+    }
 
     for (int i=0; i<=4; i++){
-        fgets(lines[i], sizeof(lines[i]), stdin);  
-        //printf("%s", lines[i]);
+        fgets(lines[i], sizeof(lines[i]),fp);  
+        //printf("%s", lines[i]);    
     }
+    
+    fclose(fp);
+
     double lat = strtod(lines[0], NULL); 
     double lon = strtod(lines[1], NULL); 
     char* tle1 = lines[2];
     char* tle2 = lines[3];  
-    double UTC = strtod(lines[4], NULL); 
+    double UTC = strtod(lines[4], NULL);
+    double el_gs = strtod(lines[5], NULL);
 
 
     vec3 satellite_position;
@@ -114,7 +123,7 @@ int main() {
     else  {printf("made it\n");}
 
 
-    point_dish(lat, lon, satellite_position, &az, &el);
-    printf("Azimuth = %lf, Elevation = %lf\n", az, el); 
+    point_dish(lat, lon, el_gs, satellite_position, &az, &el_dish);
+    printf("Azimuth = %lf, Elevation = %lf\n", az, el_dish); 
     return 0;
 }
