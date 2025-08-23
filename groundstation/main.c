2b8715d27dcd01e92f4643f0b8f72f42a7d74d0c
HistidineDwarf <dvorakdwarf@gmail.com>
2025-08-23 16:59:50 -0700

    Basic socket

--- groundstation/main.c
+++ groundstation/main.c
@@ -1,58 +1,92 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include "pointdish.h"
 
-void test_pointdish() {
-    // { echo "38.53"; echo "121.76"; curl -s 'https://tle.ivanstanojevic.me/api/tle/25544' | jq '.line1, .line2' | tr -d '"'; curl -s "https://ssd-api.jpl.nasa.gov/jd_cal.api?cd=$(date +%F_%T)" | jq -r '.jd'; } | ./pointdish
- 
-    char lines[4][100]; //pipe to here 
+// How many people we may hold in queue
+#define BACKLOG 10
 
-    double az;
-    double el_dish;
+int start_listening() {
+    struct addrinfo hints;
+    struct addrinof *result;
 
-    FILE *fp = fopen( "PS_data.txt","r");
-    if(fp == NULL){
-        perror("Error opening file");
-        return;
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+
+    // Settings
+    hints.ai_family = AF_UNSPEC;     // Don't care about v4 vs v6
+    hints.ai_socktype = SOCK_STREAM; // TCP stream socket
+    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+    // Get address or fail
+    int status;
+    if ((status = getaddrinfo(NULL, "http", &hints, &result)) != 0) {
+        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+        exit(1);
     }
 
-    for (int i=0; i<=4; i++){
-        fgets(lines[i], sizeof(lines[i]),fp);  
-        printf("%s", lines[i]);    
+    int sockfd;
+    // Walk linked list to find suitable address
+    for(struct addrinfo *p = result; p != NULL; p = p->ai_next) {
+        //Create socket
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+            p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        //See if socket used
+        int yes = 1; // Share socket if needed
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        //Bind if listening
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
     }
-    
-    fclose(fp);
+    freeaddrinfo(result); // Done with struct;
 
-    double lat = strtod(lines[0], NULL); 
-    double lon = strtod(lines[1], NULL); 
-    char* tle1 = lines[2];
-    char* tle2 = lines[3];  
-    double UTC = strtod(lines[4], NULL);
-    double el_gs = strtod(lines[5], NULL);
+    int listen_result = listen(sockfd, BACKLOG);
 
+    //If error return error. Otherwise return socket file descriptor
+    int function_result = listen_result;
+    if (!function_result) function_result = sockfd;
 
-    vec3 satellite_position;
-    if (satellite_pos(tle1, tle2, UTC, 0.0, &satellite_position) != POS_LOOKUP_SUCCESS) {
-        return -4;
-    }
-    else  {printf("made it\n");}
-
-
-    point_dish(lat, lon, el_gs, satellite_position, &az, &el_dish);
-    printf("Azimuth = %lf, Elevation = %lf\n", az, el_dish); 
-}
-
-int main() {
-    printf("Starting server\n");
-    
-    while (true) {
-        server();
-    }
-
-    return 0;
+    return function_result;
 }
 
 // Code is written with linux in mind. Compile accordingly
 // Our Pi isn't going to be running windows.
-void server()
-{
+int main() {
+    printf("Starting server\n");
     
+    int sockfd = start_listening();
+    if (!sockfd) {
+        exit(1); // error
+    }
+
+    printf("[SERVER]: Listening on 80\n");
+
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    int new_fd; // fd for the client
+    addr_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+
+    char *msg = "Hiiiiiiiii !!!\n";
+    send(new_fd, msg, strlen(msg), 0);
+
+    // while (true) {
+    //     server();
+    // }
+
+    return 0;
 }
