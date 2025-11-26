7803a466b2ec6264e7d5fefa3d9cd3958cbfb0c1
Chunho Li <lchli@ucdavis.edu>
2025-11-25 17:02:22 -0800

    Second time is the charm

--- config.mk
+++ config.mk
@@ -1,7 +1,8 @@
 CC ?= gcc
 CFLAGS ?= -MMD -MP -I.. -I.
 CFLAGS += -g
-CFLAGS += -Werror -Wall -Wextra 
+CFLAGS += -Werror -Wall 
+#CFLAGS += -Wextra 
 CFLAGS += -Wno-unused-variable -Wno-unused-value -Wno-unused-but-set-variable
 
 ifneq ($(V), 1)
