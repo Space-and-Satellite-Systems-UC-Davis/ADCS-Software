f2bc7e75f1984f137282f55969b757988aebdd4c
SorenSirius <sorenkeck@gmail.com>
2026-02-09 17:21:21 -0800

    Cleaned and validated sun sensors

--- determination/sun_sensors/sun_sensors.c
+++ determination/sun_sensors/sun_sensors.c
@@ -5,8 +5,7 @@
  * @author Soren Keck (sorenkeck@gmail.com)
  * @date 10/16/2025
  */
-
-
+#define MAX_EXPECTED_PHOTODIODE_VOLTAGE 1.62
 #define NUM_SUN_SENSORS 6
 #define pX 0
 #define nX 1
@@ -19,9 +18,9 @@
 #include <math.h>
 #include "determination/sun_lookup/sun_lookup.h"
 
-vec3 estimate_sun_photodiodes(float sensor_readings[NUM_SUN_SENSORS],vec3 *sun_vec)
+int estimate_sun_photodiodes(vec3 *sun_vec, double sensor_readings[NUM_SUN_SENSORS])
 {
-    float x_reading = 0.0, y_reading = 0.0, z_reading = 0.0;
+    double x_reading = 0.0, y_reading = 0.0, z_reading = 0.0;
     //Take either + or - for x,y,z
     if(sensor_readings[pX]>sensor_readings[nX])
     {
@@ -48,63 +47,48 @@ vec3 estimate_sun_photodiodes(float sensor_readings[NUM_SUN_SENSORS],vec3 *sun_v
         z_reading = -sensor_readings[nZ];
     }
     vec3 sun_vec_raw = (vec3){x_reading,y_reading,z_reading};
-    //Normalize the vector
-    vec_norm(sun_vec_raw,sun_vec);
+    double mag2 = x_reading*x_reading + y_reading*y_reading + z_reading*z_reading;
+    if (mag2 < 1e-12) {
+        sun_vec->x = sun_vec->y = sun_vec->z = 0.0;
+        return -1;
+    }
+    vec_norm(sun_vec_raw, sun_vec);
+    return 0;
 }
 
-
-//TODO: Implement Confidence, Convert sun_vec to ECI, determine eclipse threshold
-int is_eclipsed(float sensor_readings[NUM_SUN_SENSORS], double longitude, double latitude, double altitude,
-                             int year, int month, int day, int hour, int minute,
-                             double second)
+int is_eclipsed_photodiodes(double sensor_readings[NUM_SUN_SENSORS])
 {
-    float confidence = 0.0;
-    //Check whether photodiode totals are below a threshold
-
-    float photodiode_sum = 0.0;
-    for(int i = 0; i < NUM_SUN_SENSORS; i++)
+    double x_reading = 0.0, y_reading = 0.0, z_reading = 0.0;
+    if(sensor_readings[pX]>sensor_readings[nX])
     {
-        photodiode_sum += sensor_readings[i];
-    }
-
-    //Check whether the satellite is predicted to be in Earth's shadow
-    vec3 sun_vec = (vec3) {0.0,0.0,0.0}; 
-    sun_lookup(longitude,latitude, altitude,
-                year, month, day, hour, minute,second, &sun_vec);
-    //Need to add a conversion to bring sun_vec into ECI frame of reference
-
-    vec3 earth_vec = {0.0,0.0,0.0};
-    get_earth_direction(&earth_vec);
-    // ASSUMPTION: r_eci is just the opposite of the vector from the satellite to earth
-    vec3 r_eci = (vec3) {earth_vec.x * -1, earth_vec.y * -1, earth_vec.z * -1};
-
-    int earth_radius = 6378137;
-    double flattening_factor = 1/298.257223563;
-    double eccentricity = sqrt(2.0f*flattening_factor-powf(flattening_factor,2));
-    double mag_sun=sqrt(powf(sun_vec.x,2) + powf(sun_vec.y,2)+ powf(sun_vec.z,2));
-    double mag_sat=sqrt(powf(r_eci.x,2) + powf(r_eci.y,2) + powf(r_eci.z,2));
-    sun_vec.z=sun_vec.z/sqrt(1-powf(eccentricity,2));
-    r_eci.z=r_eci.z/sqrt(1-powf(eccentricity,2));
-    double denom = (mag_sun*mag_sun + mag_sat*mag_sat - 2 * vec_dot(sun_vec,r_eci));
-    //TODO implement division by 0 protection
-    double t_min = (mag_sun*mag_sun - vec_dot(sun_vec,r_eci))/denom;
-
-    int los = 0; 
-
-    if(t_min < 0 || t_min > 1)
-    {
-        los = 1;
+        x_reading = sensor_readings[pX];
     }
     else
     {
-       double dist_sqrd = ((1.0f-t_min)*mag_sun*mag_sun+vec_dot(sun_vec,r_eci)*t_min)/powf(earth_radius,2);
-            if (dist_sqrd > 1 )
-            {
-                los=1;
-            }
-            else
-            {
-                los = 0;
-            }
+        x_reading = -sensor_readings[nX];
+    }
+    if(sensor_readings[pY]>sensor_readings[nY])
+    {
+        y_reading = sensor_readings[pY];
+    }
+    else
+    {
+        y_reading = -sensor_readings[nY];
+    }
+    if(sensor_readings[pZ]>sensor_readings[nZ])
+    {
+        z_reading = sensor_readings[pZ];
+    }
+    else
+    {
+        z_reading = -sensor_readings[nZ];
+    }
+    double total = fabs(x_reading) + fabs(y_reading) + fabs(z_reading);
+    if(total < 0.1 * MAX_EXPECTED_PHOTODIODE_VOLTAGE)
+    {
+        return 1;
+    }
+    else{
+        return 0;
     }
 }
--- determination/sun_sensors/sun_sensors.h
+++ determination/sun_sensors/sun_sensors.h
@@ -14,15 +14,33 @@
 
 #include "adcs_math/vector.h"
 
-/**@brief Estimate the sun's position based on the readings from the photodiodes
+/**@brief Estimate the Sun direction from coarse photodiode measurements.
  *
- * @param sensor_readings The voltage values for one photodiode from each face
+ * Selects the illuminated face on each axis (+/- X,Y,Z), forms a signed
+ * vector from the three strongest axis readings, and normalizes it.
  *
- * @return sun_vec the vector pointing towards the sun from the frame of refference of the cube sat
+ * @param sensor_readings Voltage readings from one photodiode on each face
+ *                        ordered as {+X, -X, +Y, -Y, +Z, -Z}.
+ * @param sun_vec Return-by-reference pointer to normalized Sun vector in the
+ *                spacecraft body frame.
+ *
+ * @return Status code (0 = success, non-zero = insufficient signal / invalid).
  */
-vec3 estimate_sun_photodiodes(float sensor_readings[NUM_SUN_SENSORS]);
+vec3 estimate_sun_photodiodes(double sensor_readings[NUM_SUN_SENSORS]);
 
-bool is_eclipsed(
+/**@brief Detect whether the spacecraft is in eclipse using photodiode signals.
+ *
+ * Computes the total illumination from the selected illuminated face on each
+ * axis and compares it to a fixed fraction of the expected maximum photodiode
+ * voltage. If the signal is below threshold, the spacecraft is considered to
+ * be in eclipse.
+ *
+ * @param sensor_readings Voltage readings from one photodiode on each face
+ *                        ordered as {+X, -X, +Y, -Y, +Z, -Z}.
+ *
+ * @return 1 if eclipsed, 0 otherwise.
+ */
+int is_eclipsed_photodiodes(
     float sensor_readings[NUM_SUN_SENSORS]
 );
 
